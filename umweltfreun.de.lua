31e5f45a1905a4ba6fdb9e920074ef60396f6a1c
printpagestopdf <printpagestopdf@yahoo.de>
2018-03-30 22:20:12 +0200

    Create umweltfreun.de.lua

---
+++ umweltfreun.de.lua
@@ -0,0 +1,18 @@
+-- File: example.com.lua
+-- Zone: example.com
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "example.com"
+
+-- A records
+a(_a, "1.2.3.4")
+
+-- CNAME records
+cname("www", _a)
+
+-- CAA records
+caa("", "letsencrypt.org", "issue")
+caa("", "mailto:joe@example.com", "iodef")
+
+-- Templates (see templates.lua)
+google_app(_a)
