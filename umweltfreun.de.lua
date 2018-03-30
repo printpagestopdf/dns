114529e4c2072327fc9b53183d635d96a9fba601
printpagestopdf <printpagestopdf@yahoo.de>
2018-03-30 22:33:48 +0200

    Update umweltfreun.de.lua

--- umweltfreun.de.lua
+++ umweltfreun.de.lua
@@ -1,18 +1,18 @@
--- File: example.com.lua
--- Zone: example.com
+-- File: umweltfreun.de.lua
+-- Zone: umweltfreun.de
 -- SOA record is automatically generated
 -- Variable _a is replaced with zone name
--- _a = "example.com"
+-- _a = "umweltfreun.de"
 
 -- A records
-a(_a, "1.2.3.4")
+a(_a, "5.189.151.81")
 
 -- CNAME records
 cname("www", _a)
 
 -- CAA records
 caa("", "letsencrypt.org", "issue")
-caa("", "mailto:joe@example.com", "iodef")
+caa("", "mailto:webmaster@umweltinstitut.org", "iodef")
 
 -- Templates (see templates.lua)
 google_app(_a)
