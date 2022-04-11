a8cd7ff9375e6b30b9e043cc8f4c9fa3b224018b
The Ripper <printpagestopdf@yahoo.de>
2022-04-11 18:36:56 +0200

    Create rrnet.net.eu.org.lua

---
+++ rrnet.net.eu.org.lua
@@ -0,0 +1,16 @@
+-- File: umweltfreun.de.lua
+-- Zone: umweltfreun.de
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "umweltfreun.de"
+
+-- A records
+a(_a, "144.24.185.189")
+aaaa(_a, "2603:c020:8007:1100:8875:41f7:25b6:8d1a")
+
+-- CNAME records
+cname("www", _a)
+
+-- CAA records
+caa("", "letsencrypt.org", "issue")
+caa("", "mailto:printpagestopdf@yahoo.de", "iodef")
